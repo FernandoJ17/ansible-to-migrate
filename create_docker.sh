b1be6c199431c9481975578dbdcddd5e1f04235f
Fernando Javier <38330799+FernandoJ17@users.noreply.github.com>
2018-10-12 17:49:52 -0500

    Update create_docker.sh

--- create_docker.sh
+++ create_docker.sh
@@ -9,11 +9,6 @@ docker run -d -P --name server02 -p 2222:22 -p 3306:3306 $1
 echo "Servidor desplegado."
 echo
 echo "----------------------"
-echo "Lanzando servidor noSQL Redis"
-docker run -d -P --name server03 -p 2223:22 -p 6379:6379 $1
-echo "Servidor desplegado."
-echo
-echo "----------------------"
 echo "     Confirmando      "
 echo "----------------------"
 docker ps
